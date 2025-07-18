5b31116ff0a49685661d1a0ab0dd5fcf2c0116bd
Udoy Chowdhury <74410307+aontor22@users.noreply.github.com>
2025-07-18 21:32:57 +0600

    Move all files into Mobile folder

---
+++ Mobile 1/OnePlus.java
@@ -0,0 +1,217 @@
+package Mobile;
+import Login.*;
+import Payment.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*; 
+import java.awt.Color;
+import java.util.*;
+import javax.swing.ImageIcon;
+
+public class  OnePlus{
+
+	public  OnePlus()
+     {
+
+        JFrame frame = new JFrame();
+       JLabel l3 = new JLabel("OnePlus Nord N20");
+	   l3.setBounds(80,420,150,20);
+       JLabel l4 = new JLabel("OnePlus 7 Pro");
+	   l4.setBounds(330,420,100,20);
+       JLabel l5 = new JLabel("OnePlus 8 Pro");
+	   l5.setBounds(580,420,100,20);
+	   JLabel l6 = new JLabel("OnePlus 9 Pro");
+	   l6.setBounds(80,662,100,20);
+       JLabel l7 = new JLabel("Oppo A16e");
+	   l7.setBounds(330,660,100,20);
+       JLabel l8 = new JLabel("Oppo A54 ");
+	   l8.setBounds(580,660,100,20);
+	   
+	   JButton b8 = new JButton("Oppo");
+        b8.setBounds(90,110,80,20);
+        b8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new Oppo();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		
+		JButton b9 = new JButton("Vivo");
+        b9.setBounds(190,110,80,20);
+        b9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new Vivo();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		JButton b11 = new JButton("Apple");
+        b11.setBounds(290,110,80,20);
+        b11.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new Apple();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		
+		JButton b12 = new JButton("Xiaomi");
+        b12.setBounds(390,110,80,20);
+        b12.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new Xiaomi();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		JButton b13 = new JButton("Oneplus");
+        b13.setBounds(490,110,80,20);
+        b13.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new OnePlus();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		JButton b14 = new JButton("Logout");
+        b14.setBounds(590,110,80,20);
+        b14.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new Home();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		
+	   JButton b15 = new JButton("1 - 30,000");
+        b15.setBounds(170,150,200,20);
+        b15.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new  Oppo();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+		JButton b16 = new JButton(" 30,000+ ");
+        b16.setBounds(390,150,200,20);
+        b16.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new Apple();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+		
+
+        JButton b2 = new JButton(new ImageIcon("image//One7.jpg"));
+        b2.setBounds(270,220,200,200);
+        b2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new One7();
+            	frame.setVisible(false);
+               // JOptionPane.showMessageDialog(frame, "Calendar clicked");
+            }
+        });
+
+        JButton b3 = new JButton(new ImageIcon("image//Nord20.jpg"));
+        b3.setBounds(20,220,200,200);
+        b3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //JOptionPane.showMessageDialog(frame, "Clock clicked");
+				
+				new Nord20();
+            	frame.setVisible(false);
+				
+            }
+        });
+
+        JButton b4 = new JButton(new ImageIcon("image//one8.jpg"));
+        b4.setBounds(500,220,200,200);
+        b4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //JOptionPane.showMessageDialog(frame, "Contact clicked");
+				
+				new One8();
+            	frame.setVisible(false);
+				
+            }
+        });
+
+        JButton b5 = new JButton(new ImageIcon("image//one9.jpg"));
+        b5.setBounds(20,460,200,200);
+        b5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //JOptionPane.showMessageDialog(frame, "Mail clicked");
+				new One9();
+            	frame.setVisible(false);
+				
+				
+            }
+        });
+
+       JLabel l2 = new JLabel("Search here");
+	   l2.setBounds(270,50,300,20); 		
+		
+       JTextField tf1=new JTextField();  
+        tf1.setBounds(180,50,300,30);
+        JButton b1=new JButton("Search"); 
+        b1.setForeground(Color.WHITE);
+        b1.setBackground(new Color(80,80,80));		
+        b1.setBounds(500,50,150,30);
+        b1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //JOptionPane.showMessageDialog(frame, "Messages clicked");
+				//JOptionPane.showMessageDialog(frame, "Mail clicked");
+				new Search();
+            	frame.setVisible(false);
+				
+				
+            }
+        });
+        frame.add(b1);
+        frame.add(l2);
+        frame.add(tf1);
+        frame.add(b2);
+        frame.add(b3);
+        frame.add(b4);
+        frame.add(b5);
+		frame.add(b8);
+		frame.add(b9);
+		frame.add(b11);
+		frame.add(b12);
+		frame.add(b13);
+		frame.add(b14);
+		frame.add(b15);
+		frame.add(b16);
+		frame.add(l3);
+		frame.add(l4);
+		frame.add(l5);
+		frame.add(l6);
+		//frame.add(l7);
+		//frame.add(l8);
+       // frame.setLocationRelativeTo(null);
+        frame.setLayout(null);
+        //frame.getContentPane().add(BorderLayout.PAGE_START);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.decode("#FFFFFF"));
+		
+        frame.setSize(800,800);
+        frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        }
+	public static void main(String[] args) {  
+		 
+		 //new Profile("Test");
+		 //new Dashboard("Test");
+		 new OnePlus();
+		} 
+     }
+    
---
+++ Mobile 1/OppoA95.java
@@ -0,0 +1,118 @@
+
+package Mobile;
+import Payment.*;
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*; 
+import java.awt.Color;
+import java.util.*;
+public class OppoA95 {
+	
+	public  OppoA95()
+	{
+		 JFrame f = new JFrame();
+		 //f.setLocationRelativeTo(null);
+		 
+		 JLabel l1 = new JLabel(new ImageIcon("image//A95.jpg"),JLabel.CENTER);
+	     l1.setBounds(0,0,800,800);
+		 
+	     
+	        JButton b1=new JButton("<-previous page");
+	        //b1.setBackground(new Color(0,70,0));		
+	        b1.setBounds(0,110,150,20);  
+			
+	        b1.addActionListener( new ActionListener()
+			{
+				public void actionPerformed(ActionEvent ae)
+				{
+					new Oppo();
+					
+			        f.dispose();
+					
+					
+				}
+			});
+	       
+		   
+		   
+		   
+		   
+	        
+	        String[] cb4Str = { "Number Item...","1", "2", "3", "4", "5", "6", "7" };
+	        JComboBox cb4 = new JComboBox(cb4Str);
+	        cb4.setBounds(580, 100, 250, 30);
+	        cb4.setSelectedIndex(0);
+	        //cb4.setFont(f5);
+	        cb4.setBackground(Color.WHITE);
+	        
+	        
+	        JButton b3=new JButton("Buy");
+	        b3.setBackground(new Color(0,0,0));		
+	        b3.setBounds(320,620,150,20); 
+	        b3.setForeground(Color.WHITE);
+ b3.addActionListener(new ActionListener() {
+	            
+	            public void actionPerformed(ActionEvent ae) {
+	               
+	                if ((cb4.getSelectedIndex()) == 0) 
+	                {
+	                	 JOptionPane.showMessageDialog( null, "Please Select Quantity" , null, JOptionPane.INFORMATION_MESSAGE);
+	                             //new iPhone10();
+	                            // f.dispose();
+	                }
+
+                        if (cb4.getSelectedIndex() == 1) {
+                        	JOptionPane.showMessageDialog(null, "Total Cost : 2000$" ,null, JOptionPane.INFORMATION_MESSAGE);
+                        	 new Payment();
+         			          f.dispose();
+                        } 
+                        else if (cb4.getSelectedIndex() == 2) {
+                        	JOptionPane.showMessageDialog(null, "Total Cost : 3000$" ,null, JOptionPane.INFORMATION_MESSAGE);
+                        	 new Payment();
+         			        f.dispose();
+                        } 
+                        else if (cb4.getSelectedIndex() == 3) {
+                        	JOptionPane.showMessageDialog(null, "Total Cost : 4000$" ,null, JOptionPane.INFORMATION_MESSAGE);
+                        	 new Payment();
+         			        f.dispose();
+                        } 
+                        else if (cb4.getSelectedIndex() == 4) {
+                        	JOptionPane.showMessageDialog(null, "Total Cost : 5000$" ,null, JOptionPane.INFORMATION_MESSAGE);
+                        	 new Payment();
+         			        f.dispose();
+                        }
+                        else if (cb4.getSelectedIndex() == 5) {
+                        	JOptionPane.showMessageDialog(null, "Total Cost : 6000$" ,null, JOptionPane.INFORMATION_MESSAGE);
+                        	 new Payment();
+         			        f.dispose();
+                        }
+	                   
+                        
+	                }
+	            });
+	        
+	     
+	     f.add(cb4);
+	     f.add(b3);
+	     f.add(l1);
+	     f.add(b1);
+	     //f.add(b2);
+         f.setLayout(null);
+	     f.setSize(800,800);
+	     f.setVisible(true);
+		 f.setLocationRelativeTo(null);
+		 
+		 
+		 
+		 
+		 
+	}
+	public static void main(String[] args) {  
+		 
+		 //new Profile("Test");
+		 //new Dashboard("Test");
+		 new OppoA95();
+		} 
+	
+	
+}
